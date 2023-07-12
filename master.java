04e135ab3240634b54d4e1544fd4b0b6b51738f3
Maxim <maxim@harambestudios.de>
2023-07-12 02:32:00 +0200

    Added text stacking

--- master.java
+++ master.java
@@ -1,27 +1,37 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+
 public class master {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         sc.print("Willkommen bei diesem Tool!");
         String text = scanner.nextLine();
         try {
             WriteToFile(text);
-        } catch(IOException e){};
+        } catch (IOException e) {
+        }
+        ;
     }
 
-    public static void WriteToFile (String text) throws IOException
-    {
+    public static void WriteToFile(String text) throws IOException {
         File file = new File("string.txt");
-        if(!file.exists())
-        {
+        if (!file.exists()) {
             file.createNewFile();
         }
-        FileWriter writer = new FileWriter("string.txt");
-        writer.write(text);
-        writer.close();
+        Scanner reader = new Scanner(file);
+        String content = "";
+        while (reader.hasNextLine()) {
+            String data = reader.nextLine();
+            content = content + data;
+        }
+        reader.close();
+    FileWriter writer = new FileWriter("string.txt");
+    writer.write(content + text);
+    writer.close();
+
+    main(null);
     }
 }
