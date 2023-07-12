bf062438c420e745e52b6ffa7ac4e7cbda6a4aef
Tosakin <maximsanders999@gmail.com>
2023-07-12 13:58:41 +0200

    Added option to change stock and fixed potential resource leak

--- master.java
+++ master.java
@@ -9,7 +9,7 @@ public class master {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         sc.print("Welcome to CStock!");
-        sc.print("[add] or [remove] or [list]");
+        sc.print("[add] or [stock] or [remove] or [list]");
         String answer = scanner.nextLine();
         if(answer.equals("add"))
         {
@@ -26,9 +26,18 @@ public class master {
         }
         try {
             WriteFile(text, count);
-        } catch (IOException e) {
+        } catch (IOException e) {}
         }
-        ;
+        else if(answer.equals("stock"))
+        {
+            sc.print("Name the index of the item you want to change the stock of: ");
+            int index = scanner.nextInt();
+            sc.print("Name the new stock: ");
+            int stock = scanner.nextInt();
+            try{
+                ChangeStock(index, stock);
+            } catch(Exception e) {}
+
         }
         else if(answer.equals("remove"))
         {
@@ -45,6 +54,7 @@ public class master {
             }
             catch(Exception e) {}
         }
+        scanner.close();
     }
 
     public static void RemoveFile(int index) throws Exception
@@ -70,6 +80,33 @@ public class master {
         FileWriter writer = new FileWriter("string.txt");
         writer.write(result);
         writer.close();
+        reader.close();
+        main(null);
+    }
+
+    public static void ChangeStock(int index, int stock) throws Exception
+    {
+        File file = new File("string.txt");
+        Scanner reader = new Scanner(file);
+        String content = "";
+        while (reader.hasNextLine()) {
+            String data = reader.nextLine();
+            content = content + data;
+        }
+        String[] parts = content.split("%");
+        parts[index * 2] = Integer.toString(stock);
+        String result = "";
+        for(int i = 1; i < parts.length; i++)
+        {
+            if(!parts[i].equals(""))
+            {
+                result += "%" + parts[i];
+            }
+        }
+        FileWriter writer = new FileWriter("string.txt");
+        writer.write(result);
+        writer.close();
+        reader.close();
         main(null);
     }
 
@@ -87,21 +124,22 @@ public class master {
         {
             sc.print((i / 2 + 1) + "# " + parts[i] + " - " + parts[i + 1]);
         }
+        reader.close();
         main(null);
     }
 
     public static void WriteFile(String text, int count) throws IOException {
-        File file = new File("string.txt");
-        if (!file.exists()) {
-            file.createNewFile();
-        }
-        Scanner reader = new Scanner(file);
-        String content = "";
-        while (reader.hasNextLine()) {
-            String data = reader.nextLine();
-            content = content + data;
-        }
-        reader.close();
+    File file = new File("string.txt");
+    if (!file.exists()) {
+        file.createNewFile();
+    }
+    Scanner reader = new Scanner(file);
+    String content = "";
+    while (reader.hasNextLine()) {
+        String data = reader.nextLine();
+        content = content + data;
+    }
+    reader.close();
     FileWriter writer = new FileWriter("string.txt");
     writer.write(content + "%" + text + "%" + count);
     writer.close();
